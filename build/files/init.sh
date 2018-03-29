c549cef06ace5775df6c05ef0ce0e58af662db3c
Vikas Srivastava <er.vikassri@gmail.com>
2018-03-29 19:57:49 +0800

    update database/data path

--- build/files/init.sh
+++ build/files/init.sh
@@ -5,7 +5,9 @@ echo "Starting docker, this will take around 30 seconds"
 if [ -f /tmp/metastore.ack ]; then
 	if [ ! -d "/data/hive/metastore_db" ]; then
   		echo "Setting up metastore"
-  		$HIVE_HOME/bin/schematool -dbType derby -initSchema 
+        cd /data/hive
+        $HIVE_HOME/bin/schematool -dbType derby -initSchema
+        cd
   		touch /tmp/metastore.ack
 	fi
 fi
